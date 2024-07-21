621045ee08c581d73f192052cf2e2ba830e269a0
Matteo Zamana <119547209+Celadus@users.noreply.github.com>
2024-07-21 23:03:12 +0200

    proyecto asincronía

---
+++ README.md
@@ -0,0 +1,18 @@
+# LABORATORIO ASINCRONÍA
+
+En este proyecto recuperaremos los personajes de Mortadelo y Filemón de una API, los pintaremos en pantalla y, además, añadiremos una opción de buscar a los personajes por su nombre.
+
+## ¿Cómo ejecutar el proyecto?
+
+Para abrir la API:
+- Clónate este repositorio: https://github.com/Lemoncode/bootcamp-js-2
+- Ábrelo y ve a la carpeta `10-async` -> `06-lab-asincronia`
+- Instala las dependencias con `npm install`
+- Ejecuta el comando `npm start`
+
+Para abrir el proyecto:
+- Clonate el proyecto.
+- Instala las dependencias con `npm install`.
+- Ejecuta el sandbox con `npm run dev`.
+- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
+
---
+++ src/listado-personajes/listadoPersonajes.ts
@@ -0,0 +1,117 @@
+import { obtenerPersonajes } from "./listadoPersonajes.api";
+import { Personaje } from "./listadoPersonajes.model";
+
+const listadoPersonajes = document.querySelector(
+  ".listado-personajes"
+) as HTMLDivElement;
+const input = document.querySelector("#filtrar") as HTMLInputElement;
+const botonFiltrar = document.querySelector(
+  "#boton-filtrar"
+) as HTMLButtonElement;
+
+const crearElementoImagen = (
+  imagen: string,
+  nombre: string
+): HTMLImageElement => {
+  const elementoImagen = document.createElement("img");
+  elementoImagen.src = imagen;
+  elementoImagen.alt = nombre;
+
+  return elementoImagen;
+};
+
+const crearElementoParrafo = (
+  campo: string,
+  texto: string
+): HTMLParagraphElement => {
+  const parrafo = document.createElement("p");
+  parrafo.innerHTML = `<strong style="font-weight: bold">${campo}:</strong> ${texto}`;
+
+  return parrafo;
+};
+
+const crearContenedorPersonaje = (personaje: Personaje): HTMLDivElement => {
+  const divCard = document.createElement("div");
+  divCard.classList.add("personaje");
+
+  const imagen = crearElementoImagen(
+    `http://localhost:3000/${personaje.imagen}`,
+    personaje.nombre
+  );
+  divCard.appendChild(imagen);
+
+  const nombre = crearElementoParrafo("Nombre", personaje.nombre);
+  divCard.appendChild(nombre);
+
+  const apodo = crearElementoParrafo("Apodo", personaje.apodo);
+  divCard.appendChild(apodo);
+
+  const especialidad = crearElementoParrafo(
+    "Especialidad",
+    personaje.especialidad
+  );
+  divCard.appendChild(especialidad);
+
+  const habilidades = crearElementoParrafo(
+    "Habilidades",
+    personaje.habilidades.join(",")
+  );
+  divCard.appendChild(habilidades);
+
+  if (personaje.amigo) {
+    const amigo = crearElementoParrafo("Amigo", personaje.amigo);
+    divCard.appendChild(amigo);
+  }
+
+  return divCard;
+};
+
+const pintarPersonajes = async () => {
+  try {
+    const personajes = await obtenerPersonajes();
+    listadoPersonajes.innerHTML = "";
+    personajes.forEach((personaje) => {
+      const card = crearContenedorPersonaje(personaje);
+      listadoPersonajes.appendChild(card);
+    });
+  } catch (error) {
+    throw new Error("Error al pintar los personajes");
+  }
+};
+
+const filtrarPersonajes = async () => {
+  try {
+    const valorInput = input.value.toLowerCase();
+    const personajes = await obtenerPersonajes();
+    const personajesFiltrados = personajes.filter((personaje) =>
+      personaje.nombre.toLowerCase().includes(valorInput)
+    );
+    listadoPersonajes.innerHTML = "";
+    pintarPersonajesFiltrados(personajesFiltrados);
+  } catch (error) {
+    throw new Error("Error al filtrar los personajes");
+  }
+};
+
+const pintarPersonajesFiltrados = (personajes: Personaje[]) => {
+  if (listadoPersonajes && listadoPersonajes instanceof HTMLDivElement) {
+    personajes.forEach((personaje) => {
+      const card = crearContenedorPersonaje(personaje);
+      listadoPersonajes.appendChild(card);
+    });
+  } else {
+    throw new Error("Error al pintar los personajes filtrados");
+  }
+};
+
+if (botonFiltrar && botonFiltrar instanceof HTMLButtonElement) {
+  botonFiltrar.addEventListener("click", filtrarPersonajes);
+}
+
+input.addEventListener("keypress", async (event) => {
+  if (event.key === "Enter") {
+    await filtrarPersonajes();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", pintarPersonajes);
